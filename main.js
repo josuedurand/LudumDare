5e61d8b52e3e20e047d555d5f556d9b7affee87c
Josué Durand <josue.durand@greta-sud-aquitaine.academy>
2018-04-21 15:15:23 +0200

    Init projet

---
+++ main.js
@@ -0,0 +1,80 @@
+
+var character = {
+	src 		: '',
+	elem		: $('.character'),
+	position 	: {
+		top 		: 0,
+		left 		: 0
+	}
+}
+
+var colors = ['black', 'blue'];
+var $container = $('.tableau');
+var tileSize = 32;
+
+
+$(document).ready(function() {
+	initGrid();
+});
+
+
+function initGrid() {
+	for (var l = 0; l < grid.length; l++) {
+		var line = grid[l];
+		for (var c = 0; c < line.length; c++) {
+			var cell = line[c];
+			var color;
+
+			switch(cell) {
+				case 1 :
+					color = colors[1];
+					break;
+
+				default :
+					color = colors[0];
+					break;
+			}
+
+			var tile = $('<div>');
+			tile.addClass('tile');
+			tile.css('background-color', color);
+			tile.appendTo($container);
+
+			tile.css('left', (tileSize * c) + 'px');
+			tile.css('top', (tileSize * l) + 'px');
+		}
+	}
+}
+
+//renvoyer la valeur de la tile
+function getTileAt(l , c) {
+	return grid[l][c];
+}
+
+//renvoyer les tiles qui ont la valeur 1
+function getTiles(val) {
+	var tiles = [];
+
+	for (var l = 0; l < grid.length; l++) {
+		var line = grid[l];
+		for (var c = 0; c < line.length; c++) {
+			var cell = line[c];
+			if(cell == val) {
+				tiles.push({l, c});
+			}
+		}
+
+	}
+
+	return tiles;
+}
+
+//deplacer le personnage à la case l, c
+function moveCharacter(l, c) {
+	character.elem.css({
+		'top'		: l * tileSize,
+		'left'		: c * tileSize
+	});
+
+	character.position = character.elem.position();
+}
