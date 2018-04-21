e0c85a7ad0055583893fd0332246cfaf87a13ae3
Josué Durand <josue.durand@greta-sud-aquitaine.academy>
2018-04-21 15:22:49 +0200

    comment grid

--- Level.js
+++ Level.js
@@ -21,3 +21,14 @@ const grid = [
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
+
+//         Brouillard: 0
+//         Eau       : 1
+//         Chat      : 'c'
+// Poisson rouge     : 'p'
+
+// Bateau 2 (torpilleur) : 'b', 'b',
+// Bateau 3 (sous-marin) : 'b', 'b', 'b',
+// Bateau 3 (sous-marin) : 'b', 'b', 'b',
+// Bateau 4 (croiseur)   : 'b', 'b', 'b', 'b',
+// Bateau 5 (porte-avion): 'b', 'b', 'b', 'b', 'b',
